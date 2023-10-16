ee54aad5981a3fadaccc3f1101c535ef9aa87c20
D. Bohdan <dbohdan@dbohdan.com>
2023-10-16 19:54:30 +0000

    build(nox): add Python 3.12

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import nox
 
-PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11"]
+PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 
 @nox.session(python=PYTHON_VERSIONS, tags=["test"])
