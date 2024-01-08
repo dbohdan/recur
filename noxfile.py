34e47854c5fbe3b2e39dfd12304d042ca8a1254a
D. Bohdan <dbohdan@dbohdan.com>
2024-01-08 14:38:33 +0000

    build(nox): specify pytest version range (7.x)

--- noxfile.py
+++ noxfile.py
@@ -7,5 +7,5 @@ PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 @session(python=PYTHON_VERSIONS)
 def tests(session: Session) -> None:
-    session.install(".", "pytest")
+    session.install(".", "pytest>=7,<8")
     session.run("pytest")
