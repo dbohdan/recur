b9bf9257ad3afb3d7a0d928e58b968062ac593bf
D. Bohdan <dbohdan@dbohdan.com>
2024-02-12 13:45:35 +0000

    build(pytest): upgrade to major version 8

--- noxfile.py
+++ noxfile.py
@@ -7,5 +7,5 @@ PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 @session(python=PYTHON_VERSIONS)
 def tests(session: Session) -> None:
-    session.install(".", "pytest>=7,<8")
+    session.install(".", "pytest>=8,<9")
     session.run("pytest")
