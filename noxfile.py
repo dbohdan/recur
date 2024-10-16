a91224e763e4145e7f2948f7c80053c45981bf7f
D. Bohdan <dbohdan@dbohdan.com>
2024-10-16 19:37:15 +0000

    build(nox): add PyPy 3.10

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from nox import Session, session
 
-PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13"]
+PYTHON_VERSIONS = ["3.9", "3.10", "3.11", "3.12", "3.13", "pypy3.10"]
 
 
 @session(python=PYTHON_VERSIONS)
