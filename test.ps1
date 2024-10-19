d1e77b1902df91e1146999a745d041404242946b
D. Bohdan <dbohdan@dbohdan.com>
2024-10-19 17:05:27 +0000

    test: add PowerShell test script
    The script is cross-platform but added to support Windows.
    On Windows, `make test` currently fails.
    It happens because `go build -o foo`
    doesn't mean `go build -o foo.exe`.
    This is a defensible design decision for a compiler
    with strong support for cross-compilation.
    All the same, it interacts poorly with makefiles.
    Rather than work around it by introducing an `EXT` variable
    in the makefile,
    we can write a separate build plus test script.

---
+++ test.ps1
@@ -0,0 +1,27 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+$env:CGO_ENABLED = "0"
+
+$extension = ""
+if (((Get-Variable 'IsWindows' -Scope 'Global' -ErrorAction 'Ignore') -and
+     $IsWindows) -or
+    $env:OS -eq "Windows_NT") {
+    $extension = ".exe"
+}
+$build = @{
+    "recur" = "main.go"
+    "test/exit99" = "test/exit99.go"
+    "test/hello" = "test/hello.go"
+    "test/wait" = "test/wait.go"
+}
+
+foreach ($dest in $build.Keys) {
+    $executable = "$dest$extension"
+    $source = $build[$dest]
+
+    Remove-Item -Force -ErrorAction Ignore $executable
+    go build -o $executable $source
+}
+
+go test
