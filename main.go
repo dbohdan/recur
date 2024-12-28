5f66be655e32ab0a26647ce2487fd2269d75446b
D. Bohdan <dbohdan@dbohdan.com>
2024-12-28 18:29:53 +0000

    build(version): bump to 2.0.4

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ const (
 	exitCodeCommandNotFound = 255
 	exitCodeError           = -1
 	maxVerboseLevel         = 3
-	version                 = "2.0.3"
+	version                 = "2.0.4"
 )
 
 type attempt struct {
