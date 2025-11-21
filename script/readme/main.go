f160467eb66d6a770dc8d73b1cb01bada9faa57f
D. Bohdan <dbohdan@dbohdan.com>
2025-11-21 17:08:02 +0000

    build(readme): do not hard-core command

--- script/readme/main.go
+++ script/readme/main.go
@@ -1,40 +1,54 @@
 package main
 
 import (
-	"bytes"
-	"log"
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"regexp"
 	"strings"
 
 	"github.com/mitchellh/go-wordwrap"
 )
 
+const (
+	exitError    = 1
+	exitBadUsage = 2
+	filePerms    = 0o644
+	readmeFile   = "README.md"
+)
+
 func main() {
-	readmeFile := "README.md"
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s command [arg ...]\n", filepath.Base(os.Args[0]))
+		os.Exit(exitBadUsage)
+	}
+
+	command := os.Args[1:]
 
 	content, err := os.ReadFile(readmeFile)
 	if err != nil {
-		log.Fatalf("Failed to read %q: %v", readmeFile, err)
+		fmt.Fprintf(os.Stderr, "Failed to read %q: %v\n", readmeFile, err)
+		os.Exit(exitError)
 	}
 
-	var cmdOutput bytes.Buffer
-	cmd := exec.Command("./recur", "--help")
-	cmd.Stdout = &cmdOutput
+	cmd := exec.Command(command[0], command[1:]...)
 
-	if err := cmd.Run(); err != nil {
-		log.Fatalf("Failed to run command: %v", err)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to run command: %v\n", err)
+		os.Exit(exitError)
 	}
 
-	helpText := strings.TrimSpace(cmdOutput.String())
+	helpText := strings.TrimSpace(string(output))
 	wrappedHelp := wordwrap.WrapString(helpText, 80)
 
 	re := regexp.MustCompile(`(?s)<!-- BEGIN USAGE -->.*<!-- END USAGE -->`)
 	newUsageBlock := "<!-- BEGIN USAGE -->\n```none\n" + wrappedHelp + "\n```\n<!-- END USAGE -->"
 	updatedContent := re.ReplaceAllLiteralString(string(content), newUsageBlock)
 
-	if err := os.WriteFile(readmeFile, []byte(updatedContent), 0644); err != nil {
-		log.Fatalf("Failed to write %q: %v", readmeFile, err)
+	if err := os.WriteFile(readmeFile, []byte(updatedContent), filePerms); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to write %q: %v\n", readmeFile, err)
+		os.Exit(exitError)
 	}
 }
